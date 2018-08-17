380092dfcb84a0ca1c4adc71e6c2f72e40f629d5
Matt <roigmatthewe@gmail.com>
2018-08-16 20:23:14 -0400

    process.env init

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-{
+module.exports  ={
   "development": {
     "username": "root",
-    "password": "*********",
+    "password": process.env.DB_Pass,
     "database": "exampledb",
     "host": "localhost",
     "dialect": "mysql"

03f868dd86e1281e0b26584f210a506441e26e0d
Matt <roigmatthewe@gmail.com>
2018-08-23 21:44:06 -0400

    bed

--- public/js/favorites.js
+++ public/js/favorites.js
@@ -5,22 +5,25 @@ $(document).ready(function() {
     var activeUser = "101";
     var currentFavs =[];
   
-    // This function grabs updates the view and adds the 
-    const displayFavs = function(){
-        console.log("Success"+ userFavs)
-        console.log("Success"+ currentFavs)
+    // This function makes sure the favorites table is clear to avoid repeats then rebuilds the table
+    const prepTable = function(){
+        $("#favTable").empty().then(buildTable)
     }
-   
+   //
+    const buildTable = function(){
+        for (i = 0; i < currentFavs.length; i++){
+          
+        }
+   }
    //this function grabs the user id on the database and builds an array of the users favorite brew.
     const getFavs  = function() {
       $.get("/api/favs/" + activeUser, function(res) {
         console.log (res.favorites);
         str= res.favorites;
-        userFavs = str.split(",");
-        currentFavs = userFavs;
-        console.log(userFavs);
+        currentFavs = str.split(",");
+        console.log(currentFavs);
        
-      }).then(displayFavs)
+      }).then(prepTable)
     }
   
 

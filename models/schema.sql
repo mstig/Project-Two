e93995dcf1eef847ae3d8793f35808a3a4b5753f
Matt <roigmatthewe@gmail.com>
2018-08-25 07:10:52 -0400

    full function favorites table

--- models/schema.sql
+++ models/schema.sql
@@ -8,7 +8,7 @@ Select * from FavLists;
 
 Use FavBrewDB;
 INSERT INTO FavLists ( UserID, favorites, createdAt, updatedAt)
-Values ('101', '1111, 2222, 3333, 4444', NOW(), NOW()),
-('102', '1111, 2222, 3333, 4444', NOW(), NOW()),
-('103', '1111, 2222, 3333, 4444', NOW(), NOW()),
-('104', '1111, 2222, 3333, 4444', NOW(), NOW())
+Values ('101', '1, 2, 3, 4', NOW(), NOW()),
+('102', '6, 22, 7, 9', NOW(), NOW()),
+('103', '3, 4, 1, 6', NOW(), NOW()),
+('104', '8, 9, 44, 1', NOW(), NOW())
--- public/js/favorites.js
+++ public/js/favorites.js
@@ -2,37 +2,56 @@ $(document).ready(function() {
   
     // Our initial favorites array
 
-    var activeUser = "101";
+    var activeUser = "102";
     var currentFavs =[];
-  
-    // This function makes sure the favorites table is clear to avoid repeats then rebuilds the table
-    const prepTable = function(){
-        $("#favTable").empty().then(buildTable)
-    }
-   //
-    const buildTable = function(){
-        for (i = 0; i < currentFavs.length; i++){
-          
-        }
-   }
-   //this function grabs the user id on the database and builds an array of the users favorite brew.
+//this function grabs the user id on the database and builds an array of the users favorite brew converting them into integers and 
+//  sorting for future use.
     const getFavs  = function() {
       $.get("/api/favs/" + activeUser, function(res) {
-        console.log (res.favorites);
+        console.log (`Working here are User ${activeUser}'s favorites : ${res.favorites}`);
         str= res.favorites;
-        currentFavs = str.split(",");
+        currentFavs = str.split(",").map(Number);
+        currentFavs.sort(function(a, b){return a-b});
         console.log(currentFavs);
-       
-      }).then(prepTable)
+      }).then(buildFavs)
     }
-  
+// this function takes the array and builds a search string for our api call then calls the api
+    const buildFavs  = function() {
+      let x = "";
+      x = currentFavs.join('|');
+      console.log(x)
+      apiCall(x)
+      }
+// This function calls out to api with our new search term to get the data on our favorite beers
+  const apiCall = function(x){
+        $.get(`https://api.punkapi.com/v2/beers/?ids=${x}`, function(res) { 
+        console.log ("working here is first result:"+res[0].name);
+        }).then(prepTable)
+      }
 
+// This function makes sure the favorites table is clear to avoid repeats then rebuilds the table
+    const prepTable = function(res){
+        $("#favTable").empty();
+        console.log("Passing res First result: "+res[0].name);
+        buildTable(res);
+    }
+//builds the table with the data passed 
+    const buildTable = function(res){
+      for(i=0; i<currentFavs.length; i++){
+          let x = res[i];
+          var row = $('<tr>');
+          var idData = $('<td>').text(x.id);
+          var nameData = $('<td>').text(x.name).attr('scope', 'col');
+          var typeData = $('<td>').text(x.ibu).attr('scope', 'col');
+          row.append(idData);
+          row.append(nameData);
+          row.append(typeData);
+          $('#favTable').append(row);
+      }   
+   }
 
-getFavs();
-
-
-
-
+  // call get favs on page load to start our chain 
+    getFavs();
 
   });
   
